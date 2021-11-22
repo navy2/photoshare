d8699bf5f7ba0c60c19d85c4fcf8f9f39db39647
Yusuf <gman2786@gmail.com>
2021-11-22 19:32:35 +0200

    Changed cross-origin again

--- src/main/java/cmpg/photoshare/controller/MemberController.java
+++ src/main/java/cmpg/photoshare/controller/MemberController.java
@@ -18,7 +18,7 @@ import java.io.InputStream;
 import java.util.List;
 
 @Controller
-@CrossOrigin(origins = "https://photoshare-frontend.azurewebsites.net", allowedHeaders = "*", allowCredentials = "true")
+@CrossOrigin(origins = "https://photoshare-react.herokuapp.com", allowedHeaders = "*", allowCredentials = "true")
 @RequestMapping(path="/users")
 public class MemberController {
 
